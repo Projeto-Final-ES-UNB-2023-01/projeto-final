6a1393377e7ef1d33bbc645fc3eaea54018ed3f7
cadevu <carvalhocarlos233@gmail.com>
2023-07-08 13:46:36 -0300

    feat: add type a and b questions

--- app/controllers/questions_controllers.py
+++ app/controllers/questions_controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for,request
+from flask import Blueprint, render_template, redirect, url_for,request,flash
 from flask_login import login_required, current_user
 from ..models import Question
 from ..webapp import db
@@ -9,21 +9,47 @@ question = Blueprint('question',__name__)
 @login_required
 def new():
     if request.method == 'GET':
-        return render_template('questions/new.jinja2')
+        if current_user.role == 'aluno':
+            return redirect(url_for('main.profile'))
+        return render_template('questions/new_base.jinja2')
     if request.method == 'POST':
-        command = request.form.get('command')
-        answer = request.form['answer']
-        value = request.form.get('value')
+        description = request.form.get('description')
+        type = request.form.get('type')
         
-        new_question = Question(type = 'A',
-                                command = command,
-                                options = None,
-                                answer = answer,
-                                value=value,
+        new_question = Question(type = type.upper(),
+                                description = description,
                                 prof_id = current_user.id)
         db.session.add(new_question)
         db.session.commit()
 
 
-        return redirect(url_for('question.new'))
+        return redirect(url_for('question.new_type',id = new_question.id))
+    """
+    PAREI NA PARTE DE DEFINIR A FUNÇÃO NEW_NOVO TIPO E TENTAR FAZER DE UMA FORMA DINÂMICA
+    """
+    
+@question.route('/new/<id>',methods = ['POST','GET'])
+@login_required
+def new_type(id):
+    question = Question.query.filter_by(id = id).first()
+    if request.method == 'GET':
+        if current_user.role == 'aluno':
+            return redirect(url_for('main.profile'))
+        return render_template(f'questions/tipo{question.type}.jinja2')
+    if request.method == 'POST':
+        command = request.form.get('command')
+        answer = request.form['answer']
+        value = request.form.get('value')
+        
+        question.command = command
+        question.answer = answer
+        question.value = value
+        
+        
+        
+        db.session.add(question)
+        db.session.commit()
+
+
+        return redirect(url_for('main.profile'))
 
--- app/models/question.py
+++ app/models/question.py
@@ -3,6 +3,7 @@ from sqlalchemy_json import MutableJson
 class Question(db.Model):
     __tablename__ = 'questions'
     id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String)
     type = db.Column(db.String(1))
     command = db.Column(db.String)
     options = db.Column(MutableJson)
