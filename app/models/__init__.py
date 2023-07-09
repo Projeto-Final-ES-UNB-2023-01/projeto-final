4c1e022feb7c8b0facb74352d99e1c91176dd755
hrrocha0 <99548553+hrrocha0@users.noreply.github.com>
2023-07-09 20:20:53 -0300

    Removed previous models due to error

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,5 @@
 from .user import User
 from .exam import Exam
 from .question import Question
-from .answer import Answer
-from .answered_exam import AnsweredExam
 
-__all__ = [Question, User, Exam, Answer, AnsweredExam]
+__all__ = [Question, User, Exam]
